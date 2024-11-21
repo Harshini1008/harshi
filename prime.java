c439720fca78bef4292c349211267487079d1c20
Harshini1008 <harshnibellamkonda10@gmail.com>
2024-11-21 22:05:57 +0530

    har

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class Prime {
     public static void main(String[] args) {
-        int num = 98876;
+        int num = 988769888;
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
